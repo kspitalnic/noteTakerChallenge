072546727eee770119ff7b94f1b3f72b15c3984f
Kate Spitalnic <kespitalnic@gmail.com>
2022-02-04 16:37:18 -0500

    post request to add a note

--- server.js
+++ server.js
@@ -2,46 +2,78 @@ const express = require('express');
 const req = require('express/lib/request');
 const fs = require('fs');
 const path = require('path');
-const PORT = process.env.PORT || 3001; 
-const app = express () 
+const PORT = process.env.PORT || 3001;
+const app = express()
 
 //parse incoming string or array data 
 app.use(express.urlencoded({extetnded:true}));
 //parse incoming JSON data
 app.use(express.json());
 
-app.listen(PORT,() => {
-    console.log.apply(`API server now on port ${PORT}!`)
-}); 
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`)
+});
 
-const { notes } = require('./data/programs');
+const { notes } = require('./data/notes');
+
 
 function filterByQuery(query, notesArray) {
     let filteredResults = notesArray;
     if (query.title) {
-      filteredResults = filteredResults.filter(note => note.title === query.name);
+        filteredResults = filteredResults.filter(note => note.title === query.name);
     }
     if (query.content) {
-      filteredResults = filteredResults.filter(note => note.content === query.program);
+        filteredResults = filteredResults.filter(note => note.content === query.program);
     }
     return filteredResults;
-  }
+}
 
-  function findByTitle(title, notesArray) {
+function findByTitle(title, notesArray) {
     const result = notesArray.filter(note => note.title === title)[0];
     return result;
+}
+
+function createNewNote(body, notesArray){
+    const note = body;
+    clientArray.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, './data/notes.json'),
+      JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
   }
 
 
-  app.get('/api/notes', (req, res) => {
+
+
+app.get('/api/notes', (req, res) => {
     let results = notes;
     if (req.query) {
-      results = filterByQuery(req.query, notes);
+        results = filterByQuery(req.query, notes);
     }
     res.json(results);
-  });
+});
 
-  app.get('/api/notes/:title', (req, res) => {
-    const result = findByName(req.params.title, notes);
-      res.json(result);
+app.get('/api/notes/:title', (req, res) => {
+    const result = findByTitle(req.params.title, notes);
+    res.json(result);
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    //set id based on what the next index of the array will be 
+    req.body.id = notes.length.toString()
+})
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    //set id based on what the next index of the array will be 
+    req.body.id=notes.length.toString()
+
+    // if(!validateClient(req.body)){
+    //   res.status(400).send('The client is not propperly formatted');
+    //   } else { 
+    //     const client = createNewClient(req.body, clients)
+    //     res.json(req.body);
+    //   }
   });
