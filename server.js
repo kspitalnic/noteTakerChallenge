fc0016da2d7fde6583d8a106a90ba7ed2158e1aa
Kate Spitalnic <kespitalnic@gmail.com>
2022-02-04 16:16:22 -0500

    get request and function for search by params

--- server.js
+++ server.js
@@ -19,14 +19,18 @@ const { notes } = require('./data/programs');
 function filterByQuery(query, notesArray) {
     let filteredResults = notesArray;
     if (query.title) {
-      filteredResults = filteredResults.filter(notes => notes.title === query.name);
+      filteredResults = filteredResults.filter(note => note.title === query.name);
     }
     if (query.content) {
-      filteredResults = filteredResults.filter(notes => notes.content === query.program);
+      filteredResults = filteredResults.filter(note => note.content === query.program);
     }
     return filteredResults;
   }
 
+  function findByTitle(title, notesArray) {
+    const result = notesArray.filter(note => note.title === title)[0];
+    return result;
+  }
 
 
   app.get('/api/notes', (req, res) => {
@@ -35,4 +39,9 @@ function filterByQuery(query, notesArray) {
       results = filterByQuery(req.query, notes);
     }
     res.json(results);
+  });
+
+  app.get('/api/notes/:title', (req, res) => {
+    const result = findByName(req.params.title, notes);
+      res.json(result);
   });
