2b5a2588e052a851716128c4f8e26810e460e1b7
Kate Spitalnic <kespitalnic@gmail.com>
2022-02-04 20:57:33 -0500

    html and api routes file separate

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -14,16 +14,11 @@ router.post('/notes', (req, res) => {
     }
 });
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
-//Wildcard route 
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+
+module.exports = router;
